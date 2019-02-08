cae86287f521cfd4d760250815ed74c31a2cb436
Kannan Ramachandran <kannanpala@gmail.com>
2019-02-08 22:50:19 +0530

    IoT java module - bug fixes

--- PairLocator/app/App.js
+++ PairLocator/app/App.js
@@ -11,30 +11,21 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import {NativeModules} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   state = {
     message: ''
   }
 
   componentDidMount() {
-    NativeModules.IoT.getMessage((err, name) => {
-      this.setState({message: name});
+    NativeModules.IoT.pingIoT(10.0058822, 76.3066627, (err, name) => {
+      this.setState({message: err});
     });
+    
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{this.state.message}</Text>
       </View>
     );
@@ -47,15 +38,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
--- PairLocator/app/android/app/src/main/java/com/app/iot/IoTModule.java
+++ PairLocator/app/android/app/src/main/java/com/app/iot/IoTModule.java
@@ -1,11 +1,47 @@
 package com.app.iot;
 
+import com.microsoft.azure.sdk.iot.device.*;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+
+
 public class IoTModule extends ReactContextBaseJavaModule {
+    private class TelemetryDataPoint {
+        public double lat;
+        public double lng;
+    
+        // Serialize object to JSON format.
+        public String serialize() {
+          Gson gson = new Gson();
+          return gson.toJson(this);
+        }
+    }
+    
+    private class EventCallback implements IotHubEventCallback {
+        public void execute(IotHubStatusCode status, Object context) {
+            if (context != null) {
+                synchronized (context) {
+                    context.notify();
+                }
+            }
+        }
+    }
+
+    private String connString = "HostName=pair-mob-devices.azure-devices.net;DeviceId=MyPythonDevice;SharedAccessKey=RqbWt8V2QCMV04rSBwx1FhZ+a9Gyt2wCdxBWGHtC4Ek=";
+    private IotHubClientProtocol protocol = IotHubClientProtocol.MQTT;
+
     public IoTModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
@@ -16,11 +52,34 @@ public class IoTModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void getMessage(Callback cb) {
-       try{
-           cb.invoke(null, "hello world");
-       }catch (Exception e){
-           cb.invoke(e.toString(), null);
-       }
+    public void pingIoT(double lat, double lng, Callback cb) throws IOException, URISyntaxException, InterruptedException {
+        //DeviceClient client = new DeviceClient(connString, protocol);
+        //client.open();
+/*
+        TelemetryDataPoint telemetryDataPoint = new TelemetryDataPoint();
+        telemetryDataPoint.lat = lat;
+        telemetryDataPoint.lng = lng;
+
+          // Add the telemetry to the message body as JSON.
+        String msgStr = telemetryDataPoint.serialize();
+        Message msg = new Message(msgStr);
+
+        Object lockobj = new Object();
+
+        EventCallback callback = new EventCallback();
+        client.sendEventAsync(msg, callback, lockobj);
+
+        synchronized (lockobj) {
+            lockobj.wait();
+        }
+*/
+        try{
+            DeviceClient client = new DeviceClient(connString, protocol);
+            cb.invoke(null, "test hello");
+        }catch (Exception e){
+            cb.invoke(e.toString(), null);
+        }
+
+        //client.closeNow();
    }
 }
